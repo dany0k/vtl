7401800ecd886e1dba9f19c6ebc26fbf576c3cfe
shellob <smakgames.studio@gmail.com>
2026-05-20 00:54:39 +0300

    merge(feat-user-user-history): Черникова

--- VTL/history_administration/VTL_history_administration.h
+++ VTL/history_administration/VTL_history_administration.h
@@ -1,37 +1,17 @@
-#ifndef _VTL_USER_HISTORY_ADMINISTRATION_H
-#define _VTL_USER_HISTORY_ADMINISTRATION_H
+#ifndef _VTL_HISTORY_ADMINISTRATION_FACADE_H
+#define _VTL_HISTORY_ADMINISTRATION_FACADE_H
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-
-#include <VTL/user/history/VTL_user_history.h>
-#include <VTL/utils/VTL_time.h>
-#include <VTL/VTL_app_result.h>
-
-
-typedef VTL_StandartString VTL_DecryptedUserName;
-
-VTL_AppResult VTL_history_administration_ShowAll(void);
-VTL_AppResult VTL_history_administration_DownloadAll(void);
-VTL_AppResult VTL_history_administration_CopyAll(void);
-VTL_AppResult VTL_history_administration_DeleteAll(void);
-VTL_AppResult VTL_history_administration_DeleteBeforeTime(const VTL_Time* p_time);
-VTL_AppResult VTL_history_administration_DeleteAfterTime(const VTL_Time* p_time);
-VTL_AppResult VTL_history_administration_ShowBeforeTime(const VTL_Time* p_time);
-VTL_AppResult VTL_history_administration_ShowAfterTime(const VTL_Time* p_time);
-
-
-
-VTL_AppResult VTL_history_administration_ShowByUser(const VTL_DecryptedUserName user_name);
-VTL_AppResult VTL_history_administration_DeleteByUser(const VTL_DecryptedUserName user_name);
-
+#include <VTL/history_administration/VTL_history_administration_data.h>
+#include <VTL/history_administration/db/VTL_history_administration.h>
+#include <VTL/history_administration/io/VTL_history_administration_show.h>
 
 #ifdef __cplusplus
 }
 #endif
-
 
 #endif
